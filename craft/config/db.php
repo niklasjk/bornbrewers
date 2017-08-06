6395f438005ee7cf1ffeb11707fea0178e8505b5
Niklas Korpela <a1403015@myy.haaga-helia.fi>
2017-08-06 13:25:10 +0300

    updated stylings and added .gitignore

--- craft/config/db.php
+++ craft/config/db.php
@@ -6,7 +6,7 @@
  * All of your system's database configuration settings go in here.
  * You can see a list of the default settings in craft/app/etc/config/defaults/db.php
  */
- /*
+ 
 // added
 $url = getenv('JAWSDB_URL');
 $dbparts = parse_url($url);
@@ -19,7 +19,8 @@ return array(
   'database' => ltrim($dbparts['path'],'/'),
   'tablePrefix' => 'craft',
 );
-*/
+
+/** ORIGINAL
 return array(
 
 	// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
@@ -38,3 +39,4 @@ return array(
 	'tablePrefix' => 'craft',
 
 );
+*/
